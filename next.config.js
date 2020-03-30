49c0939bceabac53d12cc1a1c40d91c410e0d31b
Beto Quiroga <beto@ed.team>
2020-03-29 22:04:38 -0400

    Configura variables de entorno y dependencias

---
+++ next.config.js
@@ -0,0 +1,19 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+module.exports = phase => {
+  const isDevelopment = phase === PHASE_DEVELOPMENT_SERVER
+  const env = {
+    SITE_NAME: "EDblog",
+    API_BLOG: (() => {
+      if (isDevelopment) {
+        return "https://jsonplaceholder.typicode.com/"
+      } else {
+        return "https://api.edblog.com"
+      }
+    })()
+  }
+
+  return {
+    env,
+  }
+}
